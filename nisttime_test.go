37063812a3893636362d5fad5796259ab6d5e126
Parag Patel <parag.patel.one@gmail.com>
2014-09-09 16:55:05 -0500

    "Changed test to reflect changest in TimeNowUTC function, addtion of time struct to return

--- nisttime_test.go
+++ nisttime_test.go
@@ -6,14 +6,14 @@ import (
 )
 
 func TestTimeNowUTC(t *testing.T) {
-	time, err := TimeNowUTC()
+	tm, s, err := TimeNowUTC()
 	if err != nil {
 		t.Error(err)
 	}
 
-	if time == "" {
+	if s == "" {
 		t.Error("Server returned no data")
 	}
 
-	fmt.Println("time is => ", time)
+	fmt.Println("time is => ", tm)
 }
